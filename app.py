2bf9fbd210b63b445ef20739d8c22e9980f48147
GnanaPrakashP <itsgnanaprakash@gmail.com>
2023-02-04 15:48:01 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request,url_for,redirect,send_file
+
+app=Flask(__name__)
+
+@app.route("/",methods=["POST","GET"])
+def home():
+    return render_template("index.html")
+
+@app.route('/download')
+def download():
+	path = "walker.png"
+	return send_file(path, as_attachment=True)
+
+app.run(debug=True)
